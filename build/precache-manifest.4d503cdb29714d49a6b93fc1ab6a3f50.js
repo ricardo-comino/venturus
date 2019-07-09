c046ba7572637dd36365a6a889400ea254d127a4
Rica <ricardocomino@gmail.com>
2019-07-09 13:36:29 -0300

    Final Version with build

---
+++ build/precache-manifest.4d503cdb29714d49a6b93fc1ab6a3f50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b31c227b66c21eb64feed40c3bbd4e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33fdf8b703c72349b4d6",
+    "url": "/static/css/main.4e8ab74f.chunk.css"
+  },
+  {
+    "revision": "07a293c9e4087ec06464",
+    "url": "/static/js/2.364c424c.chunk.js"
+  },
+  {
+    "revision": "33fdf8b703c72349b4d6",
+    "url": "/static/js/main.0c23b2ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cdcc5a752e7cea999d846bd87ae202ab",
+    "url": "/static/media/logo-venturus.cdcc5a75.svg"
+  },
+  {
+    "revision": "36ee696fa9a6df9a179779a0c68e6ac6",
+    "url": "/static/media/sprite.36ee696f.svg"
+  }
+]);
